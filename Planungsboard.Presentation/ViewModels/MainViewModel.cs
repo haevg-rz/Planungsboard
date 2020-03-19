6cc819b42727a9c201151db08212da2b27bacac6
Daniel <dhcgn@users.noreply.github.com>
2020-03-19 14:21:01 +0100

    Add MVVM Light
    ref #16

---
+++ Planungsboard.Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace Planungsboard.Presentation.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+    }
+}
