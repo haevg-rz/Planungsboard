2911eade95e9863d19b4e40f33451852847b0d53
Daniel <dhcgn@users.noreply.github.com>
2020-03-21 23:21:39 +0100

    Fix filter and binding in user control

--- Planungsboard.Presentation/Converter/CardsQuarterFilter.cs
+++ Planungsboard.Presentation/Converter/CardsQuarterFilter.cs
@@ -7,17 +7,32 @@ using Planungsboard.Presentation.ViewModels;
 
 namespace Planungsboard.Presentation
 {
-    public class CardsQuarterFilter : IMultiValueConverter
+    public class CardsQuarterFilterForSingles : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var cards = values[0] as IEnumerable<Card>;
             var quarter = values[1] as string;
 
-            return cards?.Where(c => c.AssignedQuarter!= null && c.AssignedQuarter.Any(q => q == quarter)).ToList();
+            return cards?.Where(c => c.AssignedQuarter!= null && c.AssignedQuarter.Count == 1 && c.AssignedQuarter.Any(q => q == quarter)).ToList();
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CardsQuarterFilterForMultiples : IValueConverter
+    {
+        public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cards = values as IEnumerable<Card>;
+            var r = cards?.Where(c => c.AssignedQuarter != null && c.AssignedQuarter.Count > 1).ToList();
+            return r;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
