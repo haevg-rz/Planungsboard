e3738e76edd84ee538938da347842ebbdf9944e2
Daniel <dhcgn@users.noreply.github.com>
2020-04-07 22:48:59 +0200

    Save and Load
    ref #45

---
+++ Planungsboard.Presentation/ViewModels/Storage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Planungsboard.Presentation.ViewModels
+{
+    public class Storage
+    {
+        public IEnumerable<Card> BacklogCards { get; set; }
+        public IEnumerable<Card> FutureCards { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+    }
+}
